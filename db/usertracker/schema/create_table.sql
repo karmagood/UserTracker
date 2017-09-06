fbf224c20a51e8d66a7ea9ed03002898f4fce3ed
karmagood <karmagood42@gmail.com>
2017-09-06 15:48:30 +0300

    Fixed Foreign keys

--- db/usertracker/schema/create_table.sql
+++ db/usertracker/schema/create_table.sql
@@ -1,28 +1,26 @@
 CREATE TABLE IF NOT EXISTS users (
-	user_id int(100) NOT NULL AUTO_INCREMENT,
+	user_id int(10) NOT NULL AUTO_INCREMENT,
 	username varchar(255) NOT NULL,
 	email varchar(255),
 	history_path varchar(255),
-	KEY(username),	
-	PRIMARY KEY (user_id)
-);
-CREATE TABLE IF NOT EXISTS user_command (
-	username varchar(255) NOT NULL,
-	command varchar(255) NOT NULL,
-	CONSTRAINT PK_username_command PRIMARY KEY
-	(
-		username,
-		command
-	),
-	FOREIGN KEY (username) REFERENCES users (username),
-	FOREIGN KEY (command) REFERENCES commands (command), 
-	counter int
+	PRIMARY KEY (user_id),
+	KEY (username)
 );
 CREATE TABLE IF NOT EXISTS commands (
-	command_id int(100) NOT NULL AUTO_INCREMENT,
+	command_id int(10) NOT NULL AUTO_INCREMENT,
 	command varchar(255) NOT NULL,
 	threshold integer,
 	KEY(command),
 	PRIMARY KEY (command_id)
 );
-	
+CREATE TABLE IF NOT EXISTS user_command (
+	user_id int(10) NOT NULL,
+	command_id int(10) NOT NULL,
+	counter int(10),
+	PRIMARY KEY (user_id),
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+	FOREIGN KEY (command_id) REFERENCES commands(command_id)
+
+);
+
+		
