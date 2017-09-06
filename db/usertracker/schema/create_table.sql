3c34a7cc80a4210a5e33ffe927c16093a1e11f6e
karmagood <karmagood42@gmail.com>
2017-09-06 14:20:44 +0300

    added data SCHEMA

---
+++ db/usertracker/schema/create_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS users (
+	username varchar(255),
+	email varchar(255),
+	history_path varchar(255)
+);
+CREATE TABLE IF NOT EXISTS user_command (
+	username varchar(255) NOT NULL,
+	command varchar(255)  NOT NULL,
+	CONSTRAINT PK_username_command PRIMARY KEY
+	(
+		username,
+		command
+	),
+	FOREIGN KEY (username) REFERENCES users (username),
+	FOREIGN KEY (command) REFERENCES commands (command), 
+	counter int
+);
+CREATE TABLE IF NOT EXISTS commands (
+	command varchar(255),
+	threshold integer
+);
+	
